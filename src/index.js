e97b205c732c03433ca082f8c4b92d0a0243d164
Will H <will.wkhu@gmail.com>
2022-09-27 15:05:27 -0700

    Set up project
    Set proj dir structure, download dependencies/assets, add boilerplate.

---
+++ src/index.js
@@ -0,0 +1,19 @@
+import nightImg from './modules/bkgrnd/images/night.png';
+import rainImg from './modules/bkgrnd/images/rain.png';
+import snowImg from './modules/bkgrnd/images/snow.png';
+import sunImg from './modules/bkgrnd/images/sun.png';
+
+// const test = document.getElementById('content');
+// const testImg = document.createElement('img');
+// const testDiv = document.createElement('div');
+// testDiv.textContent = 'some random stuff';
+// testImg.src = bkgrndImg;
+// test.append(testImg, testDiv);
+
+// document.body.style.backgroundImage = `url(${bkgrndImg})`;
+
+// document.body.style.background = `url(${bkgrndImg}) center/cover no-repeat`;
+
+// document.body.style.background = `no-repeat center/cover rgba(255 255 128 .5) url(${bkgrndImg})`;
+
+document.body.style.background = `no-repeat center/cover url(${sunImg}) blue`;
