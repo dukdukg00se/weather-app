42236a17d75041a3aecfe1429064c4f09929b58d
Will H <will.wkhu@gmail.com>
2022-10-08 22:24:08 -0700

    Add wallpaper imgs, test fns to fetch weather info

--- main.js
+++ main.js
@@ -1,141 +1,79 @@
-/*
- * ATTENTION: The "eval" devtool has been used (maybe by default in mode: "development").
- * This devtool is neither made for production nor for readable output files.
- * It uses "eval()" calls to create a separate source file in the browser devtools.
- * If you are trying to read the output file, select a different devtool (https://webpack.js.org/configuration/devtool/)
- * or disable the default devtool with "devtool: false".
- * If you are looking for production-ready output files, see mode: "production" (https://webpack.js.org/configuration/mode/).
- */
-/******/ (() => { // webpackBootstrap
-/******/ 	"use strict";
-/******/ 	var __webpack_modules__ = ({
+const sunImg = '../src/modules/bkgrnd/images/sun.png';
+const rainImg = '../src/modules/bkgrnd/images/rain.png';
+const nightImg = '../src/modules/bkgrnd/images/night.png';
+const snowImg = '../src/modules/bkgrnd/images/snow.png';
+const highSunImg = '../src/modules/bkgrnd/images/high-sun.png';
 
-/***/ "./src/index.js":
-/*!**********************!*\
-  !*** ./src/index.js ***!
-  \**********************/
-/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+document.body.style.backgroundImage = `url(${snowImg})`;
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_bkgrnd_images_night_png__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/bkgrnd/images/night.png */ \"./src/modules/bkgrnd/images/night.png\");\n/* harmony import */ var _modules_bkgrnd_images_rain_png__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/bkgrnd/images/rain.png */ \"./src/modules/bkgrnd/images/rain.png\");\n/* harmony import */ var _modules_bkgrnd_images_snow_png__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/bkgrnd/images/snow.png */ \"./src/modules/bkgrnd/images/snow.png\");\n/* harmony import */ var _modules_bkgrnd_images_sun_png__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/bkgrnd/images/sun.png */ \"./src/modules/bkgrnd/images/sun.png\");\n\n\n\n\n\n// const test = document.getElementById('content');\n// const testImg = document.createElement('img');\n// const testDiv = document.createElement('div');\n// testDiv.textContent = 'some random stuff';\n// testImg.src = bkgrndImg;\n// test.append(testImg, testDiv);\n\n// document.body.style.backgroundImage = `url(${bkgrndImg})`;\n\n// document.body.style.background = `url(${bkgrndImg}) center/cover no-repeat`;\n\n// document.body.style.background = `no-repeat center/cover rgba(255 255 128 .5) url(${bkgrndImg})`;\n\ndocument.body.style.background = `no-repeat center/cover url(${_modules_bkgrnd_images_sun_png__WEBPACK_IMPORTED_MODULE_3__}) blue`;\n\n\n//# sourceURL=webpack://weather-app/./src/index.js?");
 
-/***/ }),
 
-/***/ "./src/modules/bkgrnd/images/night.png":
-/*!*********************************************!*\
-  !*** ./src/modules/bkgrnd/images/night.png ***!
-  \*********************************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+const searchBox = document.getElementById('search-input');
+const search = document.getElementById('search');
+let area;
 
-eval("module.exports = __webpack_require__.p + \"eabce880699534ddac31.png\";\n\n//# sourceURL=webpack://weather-app/./src/modules/bkgrnd/images/night.png?");
+searchBox.addEventListener('keypress', (e) => {
+  if (e.key === 'Enter') {
+    area = searchBox.value;
+    searchBox.value = '';
 
-/***/ }),
+    showCurrent();
+  }
+});
 
-/***/ "./src/modules/bkgrnd/images/rain.png":
-/*!********************************************!*\
-  !*** ./src/modules/bkgrnd/images/rain.png ***!
-  \********************************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+search.addEventListener('click', () => {
+  area = searchBox.value;
+  searchBox.value = ''
+});
 
-eval("module.exports = __webpack_require__.p + \"f765f503de88dea7c542.png\";\n\n//# sourceURL=webpack://weather-app/./src/modules/bkgrnd/images/rain.png?");
 
-/***/ }),
 
-/***/ "./src/modules/bkgrnd/images/snow.png":
-/*!********************************************!*\
-  !*** ./src/modules/bkgrnd/images/snow.png ***!
-  \********************************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+function showCurrent() {
+  let isZip = (/\d/g).test(area);
+  let query = isZip ? 'zip' : 'q';
+  
+  console.log(area);
+  console.log(isZip);
 
-eval("module.exports = __webpack_require__.p + \"865ac721b496d9cb4702.png\";\n\n//# sourceURL=webpack://weather-app/./src/modules/bkgrnd/images/snow.png?");
+  fetch(
+    `https://api.openweathermap.org/data/2.5/weather?${query}=${area}&APPID=0aea211463138f620add488578423899&units=imperial`,
+    { mode: 'cors' }
+  )
+    .then((response) => {
+      // console.log(response);
 
-/***/ }),
+      // JSON as input, parse it to produce a promise that resolves to JS object
+      return response.json();
+    })
+    .then((response) => {
+      // console.log(response);
 
-/***/ "./src/modules/bkgrnd/images/sun.png":
-/*!*******************************************!*\
-  !*** ./src/modules/bkgrnd/images/sun.png ***!
-  \*******************************************/
-/***/ ((module, __unused_webpack_exports, __webpack_require__) => {
+      let test = response;
+      console.log(test);
+      // img.src = response.data.images.original.url;
+      // error.textContent = '';
+    })
+    .catch(() => {
+      // error.textContent = 'No gifs found';
+    });
+}
 
-eval("module.exports = __webpack_require__.p + \"ca4b59744c710936a917.png\";\n\n//# sourceURL=webpack://weather-app/./src/modules/bkgrnd/images/sun.png?");
+// showCurrent();
+/** */
+// Logs 5 destinations with similar name
+// fetch(
+//   `https://api.openweathermap.org/geo/1.0/direct?q=san+francisco&limit=5&appid=0aea211463138f620add488578423899`,
+//   { mode: 'cors' }
+// )
+//   .then((response) => {
+//     // console.log(response);
 
-/***/ })
+//     return response.json();
+//   })
+//   .then((response) => {
+//     console.log(response);
 
-/******/ 	});
-/************************************************************************/
-/******/ 	// The module cache
-/******/ 	var __webpack_module_cache__ = {};
-/******/ 	
-/******/ 	// The require function
-/******/ 	function __webpack_require__(moduleId) {
-/******/ 		// Check if module is in cache
-/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
-/******/ 		if (cachedModule !== undefined) {
-/******/ 			return cachedModule.exports;
-/******/ 		}
-/******/ 		// Create a new module (and put it into the cache)
-/******/ 		var module = __webpack_module_cache__[moduleId] = {
-/******/ 			// no module.id needed
-/******/ 			// no module.loaded needed
-/******/ 			exports: {}
-/******/ 		};
-/******/ 	
-/******/ 		// Execute the module function
-/******/ 		__webpack_modules__[moduleId](module, module.exports, __webpack_require__);
-/******/ 	
-/******/ 		// Return the exports of the module
-/******/ 		return module.exports;
-/******/ 	}
-/******/ 	
-/************************************************************************/
-/******/ 	/* webpack/runtime/global */
-/******/ 	(() => {
-/******/ 		__webpack_require__.g = (function() {
-/******/ 			if (typeof globalThis === 'object') return globalThis;
-/******/ 			try {
-/******/ 				return this || new Function('return this')();
-/******/ 			} catch (e) {
-/******/ 				if (typeof window === 'object') return window;
-/******/ 			}
-/******/ 		})();
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/make namespace object */
-/******/ 	(() => {
-/******/ 		// define __esModule on exports
-/******/ 		__webpack_require__.r = (exports) => {
-/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
-/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
-/******/ 			}
-/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
-/******/ 		};
-/******/ 	})();
-/******/ 	
-/******/ 	/* webpack/runtime/publicPath */
-/******/ 	(() => {
-/******/ 		var scriptUrl;
-/******/ 		if (__webpack_require__.g.importScripts) scriptUrl = __webpack_require__.g.location + "";
-/******/ 		var document = __webpack_require__.g.document;
-/******/ 		if (!scriptUrl && document) {
-/******/ 			if (document.currentScript)
-/******/ 				scriptUrl = document.currentScript.src
-/******/ 			if (!scriptUrl) {
-/******/ 				var scripts = document.getElementsByTagName("script");
-/******/ 				if(scripts.length) scriptUrl = scripts[scripts.length - 1].src
-/******/ 			}
-/******/ 		}
-/******/ 		// When supporting browsers where an automatic publicPath is not supported you must specify an output.publicPath manually via configuration
-/******/ 		// or pass an empty string ("") and set the __webpack_public_path__ variable from your code to use your own logic.
-/******/ 		if (!scriptUrl) throw new Error("Automatic publicPath is not supported in this browser");
-/******/ 		scriptUrl = scriptUrl.replace(/#.*$/, "").replace(/\?.*$/, "").replace(/\/[^\/]+$/, "/");
-/******/ 		__webpack_require__.p = scriptUrl;
-/******/ 	})();
-/******/ 	
-/************************************************************************/
-/******/ 	
-/******/ 	// startup
-/******/ 	// Load entry module and return exports
-/******/ 	// This entry module can't be inlined because the eval devtool is used.
-/******/ 	var __webpack_exports__ = __webpack_require__("./src/index.js");
-/******/ 	
-/******/ })()
-;
+//     response.forEach(x => {
+//       console.log(x.name + ', ' + x.state);
+//     });
+//   })
