d3a5829affd5dca15d64655ad8325f275589fdad
Will H <will.wkhu@gmail.com>
2022-11-09 23:49:17 -0800

    Move error handling from display to controller

--- src/modules/controller.js
+++ src/modules/controller.js
@@ -1,34 +1,66 @@
 import * as display from './display';
 import * as data from './data';
 
-function setWeather(obj) {
-  data.logParams(obj);
-  display.setTheme(obj);
-  display.displayWeather(obj);
+function rmvErrOnEvnt(e) {
+  if (e.key === 'Escape' || e.type === 'click') {
+    // eslint-disable-next-line no-use-before-define
+    showSearchErr(false);
+  }
 }
 
-function listenForUserInput() {
+function showSearchErr(bool) {
+  display.searchErr(bool);
+
+  if (bool === true) {
+    document.body.addEventListener('keydown', rmvErrOnEvnt);
+    document.body.addEventListener('click', rmvErrOnEvnt);
+  } else if (bool === false) {
+    document.body.removeEventListener('keydown', rmvErrOnEvnt);
+    document.body.removeEventListener('click', rmvErrOnEvnt);
+  }
+}
+
+function handleErrs(input) {
+  if (input === 'not found') {
+    showSearchErr(true);
+  } else if (input === false) {
+    display.pageErr();
+  } else {
+    showSearchErr(false);
+  }
+}
+
+function setPage(input) {
+  handleErrs(input);
+
+  if (typeof input === 'object') {
+    data.logParams(input);
+    display.setTheme(input);
+    display.weather(input);
+  }
+}
+
+function listenForInput() {
   const searchBox = document.getElementById('search-input');
   const searchIcon = document.getElementById('search');
   const unitInput = document.getElementById('unit-input');
 
   async function getAreaWeather(e) {
     if (e.key === 'Enter' || e.type === 'click') {
-      if (this.value === '') {
+      if (this.value.length < 2) {
         return;
       }
 
       const conditions = await data.getWeather(this.value, unitInput.value);
-
-      setWeather(conditions);
+      setPage(conditions);
     }
   }
   async function updateUnits() {
     const main = document.querySelector('main');
-    const userUnits = unitInput.value == 'imperial' ? 'metric' : 'imperial';
+    const userUnits = unitInput.value === 'imperial' ? 'metric' : 'imperial';
     const conditions = await data.getWeather(main.dataset.search, userUnits);
 
-    setWeather(conditions);
+    setPage(conditions);
   }
 
   searchBox.addEventListener('keypress', getAreaWeather);
@@ -39,8 +71,8 @@ function listenForUserInput() {
 async function initWeatherPage() {
   const conditions = await data.getWeather();
 
-  setWeather(conditions);
-  listenForUserInput(conditions);
+  setPage(conditions);
+  listenForInput();
 }
 
 export default initWeatherPage;
--- src/modules/display.js
+++ src/modules/display.js
@@ -27,7 +27,7 @@ import mistIc from '../assets/images/icons/mist.svg';
 // Set wallpaper, background color that matches current weather
 function setTheme(obj) {
   if (obj) {
-    const imgCode = obj ? obj.weather[0].icon : {};
+    const imgCode = obj.weather[0].icon;
     let wallPaper;
     let wallColor;
 
@@ -89,15 +89,13 @@ function setTheme(obj) {
   }
 }
 
-// Pop weather info or display err msg
-function displayWeather(obj) {
+function weather(obj) {
   const main = document.querySelector('main');
   const city = document.querySelectorAll('.city');
   const stateCountry = document.getElementById('state-country');
   const time = document.getElementById('data-time');
   const timeZone = document.getElementById('data-zone');
   const searchBox = document.getElementById('search-input');
-  const errorMsg = document.getElementById('search-params');
   const temp = document.querySelector('#temp');
   const imperial = document.getElementById('imperial');
   const metric = document.getElementById('metric');
@@ -113,164 +111,158 @@ function displayWeather(obj) {
   const humidity = document.getElementById('humidity');
   const pressure = document.getElementById('pressure');
 
-  const showErr = () => {
-    errorMsg.style.display = 'block';
+  const regexFL = /(\b[a-z](?!\s))/g; // First letter of each word
+  const { timezone } = obj;
+  const sunriseEpoch = obj.sys.sunrise;
+  const sunsetEpoch = obj.sys.sunset;
+  const current = obj.weather[0].description;
+  const maxTemp = Math.round(obj.main.temp_max);
+  const minTemp = Math.round(obj.main.temp_min);
 
-    const removeErr = (e) => {
-      if (e.key === 'Escape' || e.type === 'click') {
-        errorMsg.style.display = 'none';
-        document.body.removeEventListener('keydown', removeErr);
-        document.body.removeEventListener('click', removeErr);
-      }
-    };
+  // Convert unix time to human readable time
+  const getHRTime = (ut, zone) => {
+    // Get date of unix timestamp
+    // Convert time (s) to ms
+    // Need to convert to ms for Date() constructor
+    // Output ex: Tue Oct 18 2022 14:34:05 GMT-0700 (Pacific Daylight Time)
+    const myDate = new Date(ut * 1000);
 
-    document.body.addEventListener('keydown', removeErr);
-    document.body.addEventListener('click', removeErr);
+    // Get time since Unix epoch 1/1/70, 00:00:00.000 GMT (in ms)
+    const myTime = myDate.getTime();
+
+    // Get difference between date in UTC time zone and local time zone (in min)
+    // Convert min to ms
+    const myOffset = myDate.getTimezoneOffset() * 60000;
+
+    // Get unix timestamp
+    const myUT = myTime + myOffset;
+
+    // Get target city unix timestamp
+    // Convert target timezone seconds to ms and add to myUT
+    const targetCityUT = myUT + 1000 * zone;
+
+    // Get human readable time
+    const destinTime = new Date(targetCityUT).toLocaleString([], {
+      hour: 'numeric',
+      minute: 'numeric',
+    });
+
+    return destinTime;
   };
 
-  if (!obj) {
-    showErr();
-  } else {
-    const regexFL = /(\b[a-z](?!\s))/g; // First letter of each word
-    const { timezone } = obj;
-    const sunriseEpoch = obj.sys.sunrise;
-    const sunsetEpoch = obj.sys.sunset;
-    const current = obj.weather[0].description;
-    const maxTemp = Math.round(obj.main.temp_max);
-    const minTemp = Math.round(obj.main.temp_min);
-
-    // Convert unix time to human readable time
-    const getHRTime = (ut, zone) => {
-      // Get date of unix timestamp
-      // Convert time (s) to ms
-      // Need to convert to ms for Date() constructor
-      // Output ex: Tue Oct 18 2022 14:34:05 GMT-0700 (Pacific Daylight Time)
-      const myDate = new Date(ut * 1000);
-
-      // Get time since Unix epoch 1/1/70, 00:00:00.000 GMT (in ms)
-      const myTime = myDate.getTime();
-
-      // Get difference between date in UTC time zone and local time zone (in min)
-      // Convert min to ms
-      const myOffset = myDate.getTimezoneOffset() * 60000;
-
-      // Get unix timestamp
-      const myUT = myTime + myOffset;
-
-      // Get target city unix timestamp
-      // Convert target timezone seconds to ms and add to myUT
-      const targetCityUT = myUT + 1000 * zone;
-
-      // Get human readable time
-      const destinTime = new Date(targetCityUT).toLocaleString([], {
-        hour: 'numeric',
-        minute: 'numeric',
-      });
-
-      return destinTime;
-    };
-
-    // Find icon for current weather, initialize alt and title text
-    const descrImgCode = obj.weather[0].icon;
-    let img;
-    let altText;
-    let titleText;
-    switch (descrImgCode) {
-      case '01d':
-        img = sunIc;
-        altText = 'Sun';
-        titleText = 'Clear sky';
-        break;
-      case '01n':
-        img = moonIc;
-        altText = 'Moon';
-        titleText = 'Clear sky';
-        break;
-      case '02d':
-        img = cloudyDIc;
-        altText = 'Sun with clouds';
-        titleText = 'Cloudy day';
-        break;
-      case '02n':
-        img = cloudyNIc;
-        altText = 'Moon with clouds';
-        titleText = 'Clear night';
-        break;
-      case '03d':
-      case '03n':
-      case '04d':
-      case '04n':
-        img = cloudyIc;
-        altText = 'Clouds';
-        titleText = 'Cloudy skies';
-        break;
-      case '09d':
-      case '09n':
-      case '10d':
-      case '10n':
-        img = rainyIc;
-        altText = 'Cloud and rain';
-        titleText = 'Rain';
-        break;
-      case '11d':
-      case '11n':
-        img = ltningIc;
-        altText = 'Cloud and lightning';
-        titleText = 'Thunderstorm';
-        break;
-      case '13d':
-      case '13n':
-        img = snowIc;
-        altText = 'Cloud and snow';
-        titleText = 'Snow';
-        break;
-      case '50d':
-      case '50n':
-        img = mistIc;
-        altText = 'Mist';
-        titleText = 'Haze';
-    }
-
-    // Highlight display units
-    switch (obj.extran.units) {
-      case 'imperial':
-        imperial.classList.add('set');
-        metric.classList.remove('set');
-        break;
-      case 'metric':
-        imperial.classList.remove('set');
-        metric.classList.add('set');
-    }
-
-    main.style.display = 'block';
-    city.forEach((header) => (header.textContent = obj.name));
-    time.textContent = getHRTime(obj.dt, obj.timezone);
-    timeZone.textContent = ` ${obj.extran.timeZone}`;
-    searchBox.value = '';
-    errorMsg.style.display = 'none';
-    temp.textContent = `${Math.round(obj.main.temp)}°`;
-    descrImg.src = img;
-    descrImg.alt = altText;
-    descrImg.title = titleText;
-    descrText.textContent = current.replace(regexFL, (fl) => fl.toUpperCase());
-    feels.textContent = `${Math.round(obj.main.feels_like)}°`;
-    sunrise.textContent = getHRTime(sunriseEpoch, timezone);
-    sunset.textContent = getHRTime(sunsetEpoch, timezone);
-    maxMin.textContent = `${maxTemp}° / ${minTemp}°`;
-    windDir.style.transform = `rotate(${obj.wind.deg}deg)`;
-    windDirTitle.textContent = `Wind ${obj.wind.deg} degrees`;
-    humidity.textContent = `${obj.main.humidity}%`;
-    pressure.textContent = `${obj.main.pressure} hPa`;
-
-    stateCountry.textContent = obj.extran.state
-      ? `${obj.extran.state}, ${obj.sys.country}`
-      : obj.sys.country;
-
-    wind.textContent =
-      obj.extran.units === 'imperial'
-        ? `${Math.round(obj.wind.speed)} mph`
-        : `${Math.round((obj.wind.speed * 3600) / 1000)} km/h`;
+  // Find icon for current weather, initialize alt and title text
+  const descrImgCode = obj.weather[0].icon;
+  let img;
+  let altText;
+  let titleText;
+  switch (descrImgCode) {
+    case '01d':
+      img = sunIc;
+      altText = 'Sun';
+      titleText = 'Clear sky';
+      break;
+    case '01n':
+      img = moonIc;
+      altText = 'Moon';
+      titleText = 'Clear sky';
+      break;
+    case '02d':
+      img = cloudyDIc;
+      altText = 'Sun with clouds';
+      titleText = 'Cloudy day';
+      break;
+    case '02n':
+      img = cloudyNIc;
+      altText = 'Moon with clouds';
+      titleText = 'Clear night';
+      break;
+    case '03d':
+    case '03n':
+    case '04d':
+    case '04n':
+      img = cloudyIc;
+      altText = 'Clouds';
+      titleText = 'Cloudy skies';
+      break;
+    case '09d':
+    case '09n':
+    case '10d':
+    case '10n':
+      img = rainyIc;
+      altText = 'Cloud and rain';
+      titleText = 'Rain';
+      break;
+    case '11d':
+    case '11n':
+      img = ltningIc;
+      altText = 'Cloud and lightning';
+      titleText = 'Thunderstorm';
+      break;
+    case '13d':
+    case '13n':
+      img = snowIc;
+      altText = 'Cloud and snow';
+      titleText = 'Snow';
+      break;
+    case '50d':
+    case '50n':
+      img = mistIc;
+      altText = 'Mist';
+      titleText = 'Haze';
   }
+
+  // Highlight display units
+  switch (obj.extran.units) {
+    case 'imperial':
+      imperial.classList.add('set');
+      metric.classList.remove('set');
+      break;
+    case 'metric':
+      imperial.classList.remove('set');
+      metric.classList.add('set');
+  }
+
+  main.style.display = 'block';
+  city.forEach((header) => (header.textContent = obj.name));
+  time.textContent = getHRTime(obj.dt, obj.timezone);
+  timeZone.textContent = ` ${obj.extran.timeZone}`;
+  searchBox.value = '';
+  temp.textContent = `${Math.round(obj.main.temp)}°`;
+  descrImg.src = img;
+  descrImg.alt = altText;
+  descrImg.title = titleText;
+  descrText.textContent = current.replace(regexFL, (fl) => fl.toUpperCase());
+  feels.textContent = `${Math.round(obj.main.feels_like)}°`;
+  sunrise.textContent = getHRTime(sunriseEpoch, timezone);
+  sunset.textContent = getHRTime(sunsetEpoch, timezone);
+  maxMin.textContent = `${maxTemp}° / ${minTemp}°`;
+  windDir.style.transform = `rotate(${obj.wind.deg}deg)`;
+  windDirTitle.textContent = `Wind ${obj.wind.deg} degrees`;
+  humidity.textContent = `${obj.main.humidity}%`;
+  pressure.textContent = `${obj.main.pressure} hPa`;
+
+  stateCountry.textContent = obj.extran.state
+    ? `${obj.extran.state}, ${obj.sys.country}`
+    : obj.sys.country;
+
+  wind.textContent =
+    obj.extran.units === 'imperial'
+      ? `${Math.round(obj.wind.speed)} mph`
+      : `${Math.round((obj.wind.speed * 3600) / 1000)} km/h`;
 }
 
-export { setTheme, displayWeather };
+function searchErr(bool) {
+  const searchErrMsg = document.getElementById('search-params');
+
+  searchErrMsg.style.display = bool === true ? 'block' : 'none';
+}
+
+function pageErr() {
+  const pageErrMsg = document.getElementById('start-err');
+  const main = document.querySelector('main');
+
+  pageErrMsg.style.display = 'flex';
+  main.style.display = 'none';
+}
+
+export { setTheme, weather, searchErr, pageErr };
